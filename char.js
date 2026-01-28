1059962cc3f52c0b3b8b6d119c0abd8d73a96052
Your Real Name <your-email@example.com>
2026-01-28 19:46:41 +0300

    The man who sold the world

--- char.js
+++ char.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
             bio: "The man who would become Big Boss. A disciple of The Boss and an expert in CQC. He famously infiltrated Tselinoyarsk to stop the Shagohod and eliminate his own mentor.",
             quote: "'I'm just a soldier. And always will be.'",
             story: "Before the legend of Big Boss, he was a green operative known as Naked Snake. During Operation Snake Eater, he was forced to kill his mentor, The Boss, to prevent a nuclear war, a task that forever changed his worldview.",
-            image: "images/METAL Gear Solid 3_ SNAKE EATER REMAKE.jpg",
+            image: "images/Naked Snake.jpg",
             video: "Video/Metal Gear Solid Delta Snake Eater - Grave Scene (Snake Crying) - Savage Slayer (720p, h264).mp4",
             audio: "Audio/Solid_Snake_MGS2_Speech.mp3.wav",
             wiki: "https://metalgear.fandom.com/wiki/Big_Boss"
