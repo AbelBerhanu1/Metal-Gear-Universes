09b85f4ce52a7e4f6f748357659802c89f4aea0b
Your Real Name <your-email@example.com>
2026-01-28 21:40:41 +0300

    minor changes in char js

--- char.js
+++ char.js
@@ -458,7 +458,7 @@ document.addEventListener("DOMContentLoaded", () => {
             bio: "A presidential candidate with extreme Social Darwinist views. He is powered by 'Nanomachines' that harden his skin in response to physical trauma.",
             quote: "'Nanomachines, son! They harden in response to physical trauma.'",
             story: "Armstrong sought to restart the American war economy to create a world where individuals fight for what they believe in, rather than for money or government orders. He is the ultimate ideological foil to Raiden.",
-            image: "images/Metal Gear Rising.jpg",
+            image: "images/Senator Armstrong.jpg",
             video: "Video/Metal Gear Rising Revengeance - R-07 Assassination Attempt Senator Armstrong & Excelsus Intro PS3 - Jason's Video Games Source (360p, h264).mp4",
             wiki: "https://metalgear.fandom.com/wiki/Steven_Armstrong"
         },
