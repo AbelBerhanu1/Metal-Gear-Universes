f691458bc8717b13a969c5eb9b84ef14712a9121
Your Real Name <your-email@example.com>
2026-01-28 17:39:33 +0300

    adding a sound function

--- char.js
+++ char.js
@@ -3,3 +3,16 @@ const SYSTEM_CONFIG = {
     lastTypingTime: 0,
     typingSoundDelay: 80
 };
+
+function playSound(id, volume = 0.3) {
+    if (!SYSTEM_CONFIG.soundEnabled) return;
+    const sound = document.getElementById(id);
+    if (!sound) {
+        console.warn(`Sound element #${id} not found in HTML.`);
+        return;
+    }
+
+    sound.currentTime = 0;
+    sound.volume = volume;
+    sound.play().catch(() => {});
+}
